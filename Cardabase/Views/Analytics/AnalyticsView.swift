1a1895d5c3692085f50007833ab431ab43157c16
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-26 13:55:32 +0900

    Add Data Import / Export function 2

---
+++ Cardabase/Views/Analytics/AnalyticsView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalyticsView.swift
+//  Cardabase
+//
+//  Created by Kenichiro Suzuki on 2026/08/25.
+//
+
+import SwiftUI
+
+struct AnalyticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalyticsView()
+}
