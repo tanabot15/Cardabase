55fd6fea599c6821256fef004561b26cd1373423
tanabot15 <kenichirosuzuki1@gmail.com>
2026-09-01 13:35:43 +0900

    Improve OnbordingView

---
+++ Cardabase/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Cardabase
+//
+//  Created by Kenichiro Suzuki on 2026/08/31.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
